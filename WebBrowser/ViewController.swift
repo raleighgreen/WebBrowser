1357e25aad9985513120a33f8b6865e687c6a1b6
Raleigh Green <raleighgreen@gmail.com>
2017-07-06 16:55:16 -0400

    web page now loads

--- WebBrowser/ViewController.swift
+++ WebBrowser/ViewController.swift
@@ -10,15 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webAddress: UITextField!
+    @IBOutlet var webView: UIWebView!
+    
+    var address: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.scalesPageToFit = true
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func goPressed(_ sender: UIButton) {
+        webAddress.resignFirstResponder()
+        loadWebPage()
     }
-
+    @IBAction func leftPressed(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    @IBAction func rightPressed(_ sender: UIBarButtonItem) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    @IBAction func zoomOut(_ sender: UIBarButtonItem) {
+        webView.scrollView.zoomScale -= 0.1
+    }
+    @IBAction func zoomIn(_ sender: UIBarButtonItem) {
+        webView.scrollView.zoomScale += 0.1
+    }
+    
+    func loadWebPage() {
+        
+        if webAddress.text != "" {
+            address = webAddress.text!
+            if address.hasPrefix("www.") {
+                address = "http://" + address
+            } else if !address.hasPrefix("http://") || !address.hasPrefix("https://") {
+                address = "http://" + address
+            }
+            
+            // url can be constructed with a string
+            let url = URL(string: address)
+            // url request will need a url
+            let request = URLRequest(url: url!)
+            // web view will load page by giving it a url request
+            webView.loadRequest(request)
+            
+            
+        }
+        
+        
+        
+    }
 
 }
 
